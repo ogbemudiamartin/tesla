ed2d40b5bcefb9b78224ef7cda30f2bfeae9cc07
martin ogbemudia <martindev52@gmail.com>
2024-01-31 21:52:18 -0700

    first commit

---
+++ asset/app.js
@@ -0,0 +1,29 @@
+
+
+var header = document.getElementById("header");
+var ms = document.getElementById("models");
+var m3 = document.getElementById("model3");
+var mx = document.getElementById("modelx");
+var my = document.getElementById("modely");
+
+
+
+
+ms.onclick = function () {
+    header.style.backgroundImage =  "url(./img/tesla\(5\).jpg)";
+}
+
+
+m3.onclick = function () {
+    header.style.backgroundImage =  "url(./img/tesla\(2\).jpg)";
+}
+
+
+mx.onclick = function () {
+    header.style.backgroundImage =  "url(./img/tesla\(3\).jpg)";
+}
+
+
+my.onclick = function () {
+    header.style.backgroundImage =  "url(./img/tesla\(4\).jpg)";
+}
